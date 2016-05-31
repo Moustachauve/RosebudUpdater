28fb2a7f0fa6cee7455dace3f071585e74a87c96
Christophe Gagnier <admin@cgagnier.ca>
2016-05-31 15:44:08 -0400

    index on master: 38c5fa5 Initial commit

---
+++ GtfsUpdater/lib/gtfs/gtfs_parse.js
@@ -0,0 +1,77 @@
+﻿var exports = module.exports = {}
+
+var gtfs_sql = require('./gtfs_sql.js')
+var fs = require('fs')
+var path = require('path')
+var log = require('../log/log.js')
+
+exports.parseFiles = function (database, directory, callback) {
+	gtfs_sql.resetTransportDatabase(database, function (err) {
+		if (err) {
+			callback(err)
+			return
+		}
+		
+		exports.getFileArray(directory, function (err, files) {
+			if (err) {
+				callback(err)
+				return
+			}
+			
+			if (files.length <= 0) {
+				log.warn('No files to import to database "' + database + '"')
+				callback()
+				return
+			}
+			
+			exports.parseFileRecursive(database, directory, files, 0, function (err) {
+				if (err) {
+					callback(err)
+					return
+				}
+
+				callback()
+			})
+		})
+	})
+}
+
+exports.parseFileRecursive = function (database, directory, fileArray, index, callback) {
+	if(index >= fileArray.length) {
+		callback()
+		return
+	}
+	
+	var currentFile = fileArray[index]
+
+	var tableName = currentFile.substr(0, currentFile.lastIndexOf('.'))
+	
+	//MySQL doesn't like double backslash and relative paths
+	var csvPath = path.dirname(require.main.filename) + '/' + directory + '/' + currentFile
+	csvPath = csvPath.replace(/\\/g, '/')
+			
+	log.verbose('Parsing file "' + currentFile + '" into "' + tableName + '"...')
+
+	gtfs_sql.injectCsvFile(database, tableName, csvPath, function (err) {
+		if (err) {
+			callback(err)
+			return
+		}
+		
+		exports.parseFileRecursive(database, directory, fileArray, index + 1, function (err) {
+			callback(err)
+			return
+		})
+	})
+}
+
+exports.getFileArray = function (directory, callback) {
+	fs.readdir(directory, function (err, files) {
+		if (err) {
+			callback(err)
+			return
+		}
+
+		callback(null, files)
+	})
+}
---
+++ GtfsUpdater/server.js
@@ -0,0 +1,16 @@
+﻿var http = require('http')
+var path = require('path')
+var gtfs = require('./lib/gtfs/gtfs.js')
+var sqlHelper = require('./lib/sqlHelper/index.js')
+var log = require('./lib/log/log.js')
+
+log.info('Starting GTFS Updater...')
+sqlHelper.init()
+
+gtfs.importAll(function (err) {
+	if (err) throw err
+	
+	log.info('GTFS Updater done working.')
+
+	process.exit()
+})
