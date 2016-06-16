8fddd91992b14b728fda199826991a06ed595b42
Moustachauve <christophegagnier@hotmail.com>
2016-06-16 16:23:09 -0400

    Add search functionnality to list of routes

--- GtfsUpdater/lib/gtfs/sql/general_schema.sql
+++ GtfsUpdater/lib/gtfs/sql/general_schema.sql
@@ -11,10 +11,12 @@ CREATE TABLE IF NOT EXISTS `feed`
 	`feed_id`				int					NOT		NULL AUTO_INCREMENT,
     `database_name`			varchar(20)			NOT		NULL,
     `url_gtfs`				varchar(100)		NOT 	NULL,
+    `keywords`				varchar(200)		NOT		NULL,
     `data_valid`			tinyint				NOT		NULL,
 	`last_edit`				timestamp			,
     `last_update`			timestamp	   NULL	DEFAULT NULL,
     `last_update_duration`	varchar(15)			DEFAULT NULL,
+    
 
 	PRIMARY KEY				(`feed_id`) 
 );
